8372d4c369bb6e0fcc40e12758f3978804cd7e4c
Thanakorn  Jaroensetthakul <35680283+pleumza2009@users.noreply.github.com>
2020-04-18 21:05:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/facebooklogin/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.facebooklogin
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import android.widget.Toast
+import com.facebook.*
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+class MainActivity : AppCompatActivity() {
+
+        var firebaseAuth : FirebaseAuth?=null
+    var callbackManager:CallbackManager?=null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        callbackManager = CallbackManager.Factory.create()
+
+        btn_login.setReadPermissions("email", "public_profile")
+        btn_login.setOnClickListener {
+
+           signIn()
+
+            val intent = Intent(this, ShowActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+
+
+   fun signIn() {
+        btn_login.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                handleFacebookAccessToken(result!!.accessToken)
+            }
+
+            override fun onCancel() {
+
+            }
+
+            override fun onError(error: FacebookException?) {
+
+            }
+
+        })
+
+
+
+
+    }
+
+    private fun handleFacebookAccessToken(accessToken: AccessToken?) {
+        val credential = FacebookAuthProvider.getCredential(accessToken!!.token)
+        firebaseAuth!!.signInWithCredential(credential)
+            .addOnFailureListener { e ->
+                Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+            }
+            .addOnSuccessListener { result ->
+                val email = result.user!!.email
+                Toast.makeText(this,"You logged with email :"+email,Toast.LENGTH_LONG).show()
+            }
+
+
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager!!.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+
+
+    fun  printKeyHash (){
+        try {
+            val info = packageManager.getPackageInfo("com.example.facebooklogin",PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures){
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.e("KEYHASH",Base64.encodeToString(md.digest(),Base64.DEFAULT))
+            }
+
+    }catch (e:PackageManager.NameNotFoundException){
+
+
+
+        }catch (e:NoSuchAlgorithmException){}
+
+    }
+
+
+}
+
+
+
+
+
---
+++ app/src/main/java/com/example/facebooklogin/ShowActivity.kt
@@ -0,0 +1,12 @@
+package com.example.facebooklogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show)
+    }
+}
